be4e6885779e29a3600468bd9b6e77e30b556d59
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-03-16 16:26:43 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValues){
+    if(someValues<42){
+        return(42-someValues);
+    }
+    else{
+        return(someValues-42)
+    }
+}
+function distanceFromHqInFeet(someValues){
+    return distanceFromHqInBlocks(someValues)*264;
+}
+function distanceTravelledInFeet(a,b){
+    if(b>a){
+        return((b-a)*264);
+    }
+    else
+    return((a-b)*264)
+}
+function calculatesFarePrice(start,destination){
+    let distance = distanceTravelledInFeet(start,destination);
+    if(distance<= 400){
+        return 0;
+    }
+    if(distance>=400 && distance<=2000){
+        return(distance-400)*0.02;
+    }
+    else if(distance>=2000&&distance<=2500){
+        return 25;
+    }
+    else if(distance > 2500){
+        return "cannot travel that far";
+    }
+}
